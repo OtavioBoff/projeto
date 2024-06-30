fd9c1d3b7de160048739de980be0d86a98181d75
Otavio Boff <boffotavio2005@gmail.com>
2024-06-29 22:25:44 -0300

    first commit

---
+++ src/components/Comment.jsx
@@ -0,0 +1,32 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/diego3g.png" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Diego Fernandes</strong>
+              <time title="11 de Maio às 08:13h" dateTime="2024-06-29 18:05:00">
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+          <p>Muito bom Devon, Parabéns!! clapclap</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp size={20} />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Post.jsx
@@ -0,0 +1,48 @@
+import { Comment } from "./Comment";
+import styles from "./Post.module.css";
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/MarceloHBoff.png"
+          />
+          <div className={styles.authorInfo}>
+            <strong>MarceloHBoff</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        <time title="11 de Maio às 08:13h" dateTime="2024-06-29 18:05:00">
+          Publicado há 1h
+        </time>
+      </header>
+      <div className={styles.content}>
+        <p>É os guri do grêmio</p>
+        <p>Não tem como</p>
+        <p>
+          Projetin felas{" "}
+          <a href="https://github.com/MarceloHBoff">github.com/MarceloHBoff</a>
+        </p>
+        <p>
+          <a href="">#Sextou </a>
+          <a href="">#SóPeloGole </a>
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe um cometário aqui" />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
+    </article>
+  );
+}
